9a418f3ed70394e30b3cf7fa7acb9063c871f3d8
kalebmelaku <kalebmelaku97@hotmail.com>
2024-06-22 01:45:35 +0300

    404

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import HomePage from "./pages/homePage";
 import LoginPage from "./pages/LoginPage";
 import ConfirmPage from "./pages/confirmPage";
 import Admin from "./pages/admin";
+import NotFound from "./pages/404";
 function App() {
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path="/" element={<LoginPage />} />
+					<Route path="/" element={<NotFound />} />
 					<Route path="/confirm" element={<ConfirmPage />} />
 					<Route path="/homePage" element={<HomePage />} />
 					<Route path="/admin" element={<Admin />} />
